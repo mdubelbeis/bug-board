6bacf457cd08a0fc251eb822e87df3e69e0439a5
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-05-21 12:31:00 -0500

    feat: add comment routes and controller

---
+++ server/src/controllers/commentController.js
@@ -0,0 +1,89 @@
+import Comment from '../models/commentModel.js';
+
+export const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        comments,
+      },
+    });
+  } catch (err) {}
+};
+
+export const createComment = async (req, res) => {
+  try {
+    const comment = await Comment.insertOne(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        comment,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+export const findCommentById = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        comment,
+      },
+    });
+  } catch (err) {
+    res.send(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+export const updateComment = async (req, res) => {
+  try {
+    const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        comment,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+export const deleteComment = async (req, res) => {
+  try {
+    const comment = await Comment.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        comment,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import {
+  createComment,
+  deleteComment,
+  findCommentById,
+  getAllComments,
+  updateComment,
+} from '../controllers/commentController.js';
 
 const router = express.Router();
 
-router.route('/');
-router.route('/:id');
+router.route('/').get(getAllComments).post(createComment);
+router
+  .route('/:id')
+  .get(findCommentById)
+  .delete(deleteComment)
+  .patch(updateComment);
 
 export default router;
