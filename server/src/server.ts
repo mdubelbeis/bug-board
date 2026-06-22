74325646cef6271403a1fe65867b86d012327b6a
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-06-22 16:42:07 -0500

    update db connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,18 +7,21 @@ const PORT = process.env.PORT || 8000;
 
 const { default: app } = await import('./app.js');
 
-const DB_CLOUD = process.env.DB_CLOUD;
-const DB_LOCAL = process.env.DB_LOCAL;
+const DB =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_CLOUD
+    : process.env.DB_LOCAL;
 
-if (!DB_CLOUD || !DB_LOCAL) {
-  throw new Error('DB_CLOUD or DB_LOCAL is not defined');
+if (!DB) {
+  throw new Error('Database connection string is not defined');
 }
 
 mongoose
-  .connect(DB_LOCAL)
-  .then((conn) => {
+  .connect(DB)
+  .then(() => {
     console.log('Successful database connection');
     console.log('Using database:', mongoose.connection.name);
+
     app.listen(PORT, () => {
       console.log(`Server listening on port: ${PORT}`);
     });
